e0822d76c9e1df2d2816c67e24eb4d52d141fe4a
zhongzhi107 <zhong.zhi@163.com>
2016-07-22 19:04:43 +0800

    Add comments

--- config/packing.js
+++ config/packing.js
@@ -3,27 +3,41 @@ export default {
   path: {
     // 源文件目录
     src: 'src',
-    // 静态文件目录
+
+    // 静态文件目录，可以设置在src里，也可以设置在src外
     assets: 'static',
+
     // 页面初始化mock数据文件存放目录
     mockPageInit: 'mock/pages',
+
     // webpack打包入口JS文件目录
     entries: 'src/entries/html/{pagename}.js',
-    // 模版目录
+
+    // 模版目录，如果模版支持继承或layout的话
+    // 模板一般会再区分布局文件(layout)和网页文件(pages)
     templates: 'src/templates/html',
+
+    // 模版网页文件，如果没有使用layout的话，保持这个地址和`templates`一样
     templatesPages: 'src/templates/html',
+
     // 编译输出产物目录
     dist: 'prd',
+
     // 编译后的静态文件目录
     assetsDist: 'prd/assets',
-    // 编译后的模版目录
+
+    // 编译后的模版目录，如果模版支持继承或layout的话
+    // 模板一般会再区分布局文件(layout)和网页文件(pages)
     templatesDist: 'prd/templates',
+
+    // 编译后的模版网页文件，如果没有使用layout的话，保持这个地址和`templatesDist`一样
     templatesDistPages: 'prd/templates'
   },
 
-  // 模版类型
+  // 模版类型，用数组形式支持多种模版，如 ['html', 'jade']
+  // 新增模版后，需要在webpack配置中加上对应的loader
   templateLoader: 'html',
-  // 模版文件扩展名
+  // 模版文件扩展名，用数组形式支持多种模版，如 ['.html', '.jade']
   templateExtension: '.html',
 
   // webserver端口
@@ -36,7 +50,9 @@ export default {
 
   commonChunks: {
     // CommonsChunkPlugin会将最后一个当作Entry chunk
-    // Todo: 通过参数控制Entry chunk
+    // 注意，如果配置了commonChunks，所有网页模版需要引用公共包文件
+    // 否则会报错
+    // <script src="/vendor.js"></script>
     vendor: [
       'react',
       'react-dom',
