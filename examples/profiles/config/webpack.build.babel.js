ea5ee06bfc0252097ff342d68b54fa8519191120
zhongzhi107 <zhong.zhi@163.com>
2016-11-01 18:36:48 +0800

    add ProfilesPlugin

---
+++ examples/profiles/config/webpack.build.babel.js
@@ -0,0 +1,21 @@
+/**
+ * 这个文件可以修改serve的默认设置
+ * 配置文件的位置在 `node_modules/packing/config/webpack.serve.babel.js`
+ *
+ * @param object webpackConfig 默认配置对象
+ */
+
+import assign from 'object-assign-deep';
+import ProfilesPlugin from 'packing-profile-webpack-plugin';
+
+export default (webpackConfig) => {
+
+  return assign({}, webpackConfig, {
+    // 在这里自定义配置
+    plugins: webpackConfig.plugins.concat(
+      new ProfilesPlugin({
+        failOnMissing: true,
+      }),
+    ),
+  });
+};
