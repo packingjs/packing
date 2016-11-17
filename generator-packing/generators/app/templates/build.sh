1711b506de0af505c8165c59167f46729b5868f0
zhongzhi107 <zhong.zhi@163.com>
2016-11-18 05:25:59 +0800

    npm-cache

--- generator-packing/generators/app/templates/build.sh
+++ generator-packing/generators/app/templates/build.sh
@@ -6,7 +6,7 @@ if [ ! -n "$1" ] ;then
   echo 参数错误
   echo usage: build.sh target_name
 else
-  npm install --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass
+  NPM_CACHE_DIR=/home/q/prj/npm npm-cache install npm --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass
   echo target_name: $1
   npm run build:$1
 fi
