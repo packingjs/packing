4dcf5f720432646c27d6521f38cb206127e39f36
zhongzhi107 <zhong.zhi@163.com>
2016-12-26 05:51:07 +0800

    add default home page router

--- config/packing.js
+++ config/packing.js
@@ -111,7 +111,7 @@ export default {
   // require! 表示使用本地mock文件
   rewriteRules: {
     // 网站URL与模版的对应路由关系
-    // '^/$': '/index.html',
+    '^/$': '/index.html',
 
     // API转发
     '^/api/(.*)': 'require!/mock/api/$1.js',
