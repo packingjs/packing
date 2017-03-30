ba1c14b2717234a94e45be005ad7887dbbf287d7
zhongzhi107 <zhong.zhi@163.com>
2017-03-30 10:42:32 +0800

    chore: log more info

--- bin/packing.js
+++ bin/packing.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const pkg = require('../package.json');
+
+console.log('Versions:');
+console.log(` user-agent: ${process.env.npm_config_user_agent}`);
+console.log(` packing: ${pkg.version}`);
 
 program
   .command('serve', 'start dev web server', { isDefault: true })
