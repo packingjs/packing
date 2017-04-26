dd97fb7fdb36a2173d2e9e1e0c05ccecb6f4ea12
zhongzhi107 <zhong.zhi@163.com>
2017-04-26 15:51:31 +0800

    chore: Updated webpack config template

--- generator-packing/generators/app/templates/config/webpack.serve.babel.js
+++ generator-packing/generators/app/templates/config/webpack.serve.babel.js
@@ -6,16 +6,17 @@
  */
 
 export default webpackConfig => {
+  const config = webpackConfig;
   // webpackConfig 为系统默认的webpack配置，此处可以根据项目情况修改
   // 修改 entry
-  // webpackConfig.entry = 'xxx/xxx.js';
+  // config.entry = 'xxx/xxx.js';
   // 修改 plugins（修改 ＝ 先删除现有的，再添加新的）
-  // webpackConfig.plugins = webpackConfig.plugins.filter(plugin => !(plugin.filename && plugin.id));
-  // webpackConfig.plugins.push(
+  // config.plugins = config.plugins.filter(plugin => !(plugin.filename && plugin.id));
+  // config.plugins.push(
   //   new ExtractTextPlugin({
   //     filename: '[name]-[contenthash:8].css',
   //     allChunks: true
   //   })
   // )
-  return webpackConfig;
+  return config;
 };
