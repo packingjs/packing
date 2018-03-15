7e858282bc0d77dcd85904f86775f6bd9e9f260a
zhongzhi107 <zhong.zhi@163.com>
2018-03-15 19:33:02 +0800

    test(html-template-require): 增加 node_modules 内文件引用的测试用例

--- test/cases/html-template-require/__.js
+++ test/cases/html-template-require/__.js
@@ -2,6 +2,7 @@
 // 后续整合到 packing-template-plugin 中
 // 代码自动完成资源的引用
 
+import './node_modules/3.png';
 import './1.jpg';
 import './images/1.jpg';
 import './images/2.jpg';
--- test/cases/html-template-require/test.js
+++ test/cases/html-template-require/test.js
@@ -1,10 +1,16 @@
 import { readFileSync, existsSync } from 'fs';
 import rimraf from 'rimraf';
-import { exec, getTestCaseName } from '../../util';
+import { exec, getTestCaseName, random } from '../../util';
 
-describe(getTestCaseName(), async () => {
+process.env.NODE_ENV = random() ? 'local' : 'production';
+
+describe(`${getTestCaseName()}(${process.env.NODE_ENV})`, async () => {
   let html;
+  let publicPath;
   before(async () => {
+    publicPath = process.env.NODE_ENV === 'local' ?
+      '/' :
+      '//q.qunarzz.com/__xxxx__/prd/assets/';
     const cmd = 'node_modules/.bin/babel-node src/bin/packing.js build';
     if (process.env.DEBUG) {
       const stdout = await exec(cmd);
@@ -28,32 +34,32 @@ describe(getTestCaseName(), async () => {
   });
 
   it('应该正确替换网页中的一级目录图片地址', async () => {
-    html.should.match(/src="\/1_\w{8}.jpg"/);
+    html.should.match(new RegExp(`src="${publicPath}1_\\w{8}.jpg"`));
   });
 
   it('应该正确替换网页中的二级目录图片地址', async () => {
-    html.should.match(/src="\/images\/2_\w{8}.jpg"/);
+    html.should.match(new RegExp(`src="${publicPath}images/2_\\w{8}.jpg"`));
   });
 
   it('应该正确替换不同目录下同名的图片地址', async () => {
-    html.should.match(/src="\/images\/1_\w{8}.jpg"/);
+    html.should.match(new RegExp(`src="${publicPath}images/1_\\w{8}.jpg"`));
   });
 
   it('网络图片应该保持原有网络地址', async () => {
-    html.should.match(/src="\/\/qzz.com\/images\/2.jpg"/);
+    html.should.match(new RegExp('src="//qzz.com/images/2.jpg"'));
   });
 
-  it('应该替换同一文件中多次引用的图片地址', async () => {
-    const matches = html.match(/src="\/1_\w{8}.jpg"/g);
+  it('应该替换多次引用的图片地址', async () => {
+    const matches = html.match(new RegExp(`src="${publicPath}1_\\w{8}.jpg"`, 'g'));
     matches.should.have.length(2);
   });
 
   it('应该替换 favicon 图片地址', async () => {
-    html.should.match(/href="\/images\/favico_\w{8}.jpg"/);
+    html.should.match(new RegExp(`href="${publicPath}images/favico_\\w{8}.jpg"`));
   });
 
   it('应该替换 img 标签中的 data-src 属性', async () => {
-    html.should.match(/data-src="\/images\/2_\w{8}.jpg"/);
+    html.should.match(new RegExp(`data-src="${publicPath}images/2_\\w{8}.jpg"`));
   });
 
   it('应该不替换 img 标签中的 alt 属性', async () => {
@@ -61,7 +67,11 @@ describe(getTestCaseName(), async () => {
   });
 
   it('应该替换所有标签中的 test 属性', async () => {
-    const matches = html.match(/test="\/images\/2_\w{8}.jpg"/g);
+    const matches = html.match(new RegExp(`test="${publicPath}images/2_\\w{8}.jpg"`, 'g'));
     matches.should.have.length(2);
+  });
+
+  it('应该替换 node_modules 中的图片', async () => {
+    html.should.match(new RegExp(`src="${publicPath}node_modules/3_\\w{8}.png"`));
   });
 });
