c86ac8e83a562b43523dc279519f8211a6654527
zhongzhi107 <zhong.zhi@163.com>
2018-03-28 10:46:59 +0800

    chore(test): 测试失败时能返回失败代码

--- test/index.js
+++ test/index.js
@@ -6,22 +6,29 @@ const path = require('path');
 const files = glob.sync('cases/**/index.js', { cwd: __dirname });
 files.sort((a, b) => path.dirname(a) > path.dirname(b));
 
-let failed = 0;
+const errors = [];
 files.forEach((file, i) => {
   const cmd = `node_modules/.bin/mocha test/${file}`;
-  console.log(chalk.yellow(`[${i + 1}/${files.length}] ${cmd}`));
+  const title = `[${i + 1}/${files.length}] ${cmd}`;
+  console.log(chalk.yellow(title));
   try {
     const stdout = cp.execSync(cmd, { encoding: 'utf-8' });
     console.log(stdout);
   } catch (e) {
-    failed += 1;
-    // console.log(e);
+    errors.push(title);
     console.log(chalk.red(e.stdout));
   }
 });
 
-console.log('done');
+const output = ['done:'];
 
-if (failed > 0) {
-  console.log(chalk.red(`${failed} failed`));
+if (errors.length === 0) {
+  output.push('  all passing');
+  console.log(output.join('\n'));
+} else {
+  output.push(`  ${files.length - errors.length} passing`);
+  output.push(chalk.red(`  ${errors.length} failing`));
+  errors.forEach(title => output.push(chalk.red(`    - ${title}`)));
+  console.log(output.join('\n'));
+  process.exit(1);
 }
