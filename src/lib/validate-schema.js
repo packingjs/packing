5d255cf90bebe0ba1e8aed18e2850c98769a3f04
zhi.zhong <zhong.zhi@163.com>
2020-10-16 17:48:35 +0800

    chore: 使用新的参数校验方式

--- src/lib/validate-schema.js
+++ src/lib/validate-schema.js
@@ -1,28 +1,18 @@
-// import validateSchema from 'webpack/lib/validateSchema';
-// import WebpackOptionsValidationError from 'webpack/lib/WebpackOptionsValidationError';
-// import packingOptionsSchema from '../schemas/packing-options';
-// import { pRequire } from '..';
-
-// class PackingOptionsValidationError extends Error {
-//   constructor(message) {
-//     super();
-//     this.name = 'PackingOptionsValidationError';
-//     this.message = message;
-//   }
-// }
+import { validateSchema } from 'webpack/lib';
+import packingOptionsSchema from '../schemas/packing-options';
+import { pRequire } from '..';
 
 /**
  * 校验 `config/packing.js` 参数
  */
 export default () => {
-  // const options = pRequire('config/packing');
-  // const packingOptionsValidationErrors = validateSchema(packingOptionsSchema, options);
-
-  // if (packingOptionsValidationErrors.length !== 0) {
-  //   // 重写 error.message
-  //   // 保证输出消息的可读性
-  //   // const error = new WebpackOptionsValidationError(packingOptionsValidationErrors);
-  //   // const message = error.message.replace(/Webpack/mg, 'Packing');
-  //   throw new PackingOptionsValidationError(packingOptionsValidationErrors);
-  // }
+  const options = pRequire('config/packing');
+  try {
+    validateSchema(packingOptionsSchema, options);
+  } catch (e) {
+    // 改写默认异常信息
+    e.headerName = 'Packing';
+    e.message = e.message.replace(/Webpack/mg, 'Packing');
+    console.log(e);
+  }
 };
